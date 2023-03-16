09459a87a233b28e22663a8093370945bcca180c
Code-nano <83939407+Code-nano@users.noreply.github.com>
2023-03-16 20:10:02 +0800

    updated

--- xml_to_tfrecord.py
+++ xml_to_tfrecord.py
@@ -15,10 +15,10 @@ Example:
 """
 """
 Usage in Jupyter Notebook:
-    %run xml_to_tfrecord.py -traini /path/to/train/images -testi /path/to/test/images -trainxml /path/to/train/xmls -testxml /path/to/test/xmls -output /path/to/output/folder -label_map /path/to/label_map.pbtxt
+    !pyhton xml_to_tfrecord.py -traini /path/to/train/images -testi /path/to/test/images -trainxml /path/to/train/xmls -testxml /path/to/test/xmls -output /path/to/output/folder -label_map /path/to/label_map.pbtxt
 
 Example:
-    %run xml_to_tfrecord.py -traini /data/train/images -testi /data/test/images -trainxml /data/train/xmls -testxml /data/test/xmls -output /data/output -label_map /data/label_map.pbtxt
+    !python xml_to_tfrecord.py -traini /data/train/images -testi /data/test/images -trainxml /data/train/xmls -testxml /data/test/xmls -output /data/output -label_map /data/label_map.pbtxt
 """
 """
 TF_RECORD_SCRIPT = files['TF_RECORD_SCRIPT']
@@ -28,7 +28,7 @@ TRAIN_XML_DIR = os.path.join(paths['ANNOTATION_PATH'], 'train')
 TEST_XML_DIR = os.path.join(paths['ANNOTATION_PATH'], 'test')
 OUTPUT_DIR = paths['ANNOTATION_PATH']
 
-%run {TF_RECORD_SCRIPT} -traini {TRAIN_IMAGE_DIR} -testi {TEST_IMAGE_DIR} -trainxml {TRAIN_XML_DIR} -testxml {TEST_XML_DIR} -output {OUTPUT_DIR} -label_map {LABELMAP_DIR}
+!python {TF_RECORD_SCRIPT} -traini {TRAIN_IMAGE_DIR} -testi {TEST_IMAGE_DIR} -trainxml {TRAIN_XML_DIR} -testxml {TEST_XML_DIR} -output {OUTPUT_DIR} -label_map {LABELMAP_DIR}
 """
 
 
@@ -41,29 +41,35 @@ from object_detection.utils import dataset_util, label_map_util
 import io
 from PIL import Image
 import argparse
+from collections import namedtuple
+
 
 def xml_to_csv(path):
     xml_list = []
-    for root, dirs, files in os.walk(path):
+    for root_dir, dirs, files in os.walk(path):
         for file in files:
             if file.endswith('.xml'):
-                xml_file = os.path.join(root, file)
+                xml_file = os.path.join(root_dir, file)
                 tree = ET.parse(xml_file)
-                root = tree.getroot()
+                xml_root = tree.getroot()
 
-                for member in root.findall('object'):
-                    value = (root.find('filename').text,
-                             int(root.find('size')[0].text),
-                             int(root.find('size')[1].text),
+                for member in xml_root.findall('object'):
+                    relative_path = os.path.relpath(os.path.dirname(xml_file), path) if os.path.dirname(xml_file) != '' else '.'
+                    if not relative_path:  
+                        relative_path = '.'  
+                    value = (xml_root.find('filename').text,
+                             int(xml_root.find('size')[0].text),
+                             int(xml_root.find('size')[1].text),
                              member[0].text,
                              int(member[4][0].text),
                              int(member[4][1].text),
                              int(member[4][2].text),
                              int(member[4][3].text),
-                             os.path.relpath(os.path.dirname(xml_file), path))  # Update this line
+                             relative_path)
                     xml_list.append(value)
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax', 'relative_path']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
+    print(f"XML data:\n{xml_df}")
     return xml_df
 
 
@@ -122,25 +128,36 @@ def create_tf_example(group, path):
     }))
     return tf_example
 
+def split(df, group_key):
+    data = namedtuple('data', ['filename', 'object'])
+    gb = df.groupby(group_key)
+    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
+
 def generate_tfrecords(image_dir, csv_input, output_path):
     writer = tf.io.TFRecordWriter(output_path)
+    path = os.path.join(image_dir)
     examples = pd.read_csv(csv_input)
-    
-    # Check for missing values in the 'relative_path' column
-    missing_values = examples['relative_path'].isna()
-    if missing_values.any():
-        print("Warning: Missing values found in the 'relative_path' column:")
-        print(examples[missing_values])
+    grouped = split(examples, 'filename')
 
-    grouped = examples.groupby(['filename', 'relative_path'])
+    processed = 0  # Add this line
+    missing_images = 0
 
-    for group_keys in grouped.groups.keys():
-        group_data = grouped.get_group(group_keys)
-        tf_example = create_tf_example(group_data, image_dir)
-        writer.write(tf_example.SerializeToString())
+    for group_keys, group_data in grouped:
+        for index, row in group_data.iterrows():
+            try:
+                tf_example = create_tf_example(row, image_dir)
+                writer.write(tf_example.SerializeToString())
+                processed += 1  # Add this line
+                print(f"Processed {processed} records")  # Add this line
+            except FileNotFoundError:
+                missing_images += 1
+                print(f"Missing image: {row['filename']}")
 
     writer.close()
-    print(f'Successfully created the {output_path} file')
+    print(f"Successfully created the TFRecords: {output_path}")
+    print(f"Processed {processed} records")  # Add this line
+    print(f"Missing images: {missing_images}")  # Add this line
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert XML files to TensorFlow Record files')
@@ -160,8 +177,17 @@ if __name__ == '__main__':
     train_csv = xml_to_csv(args.train_xml_dir)
     test_csv = xml_to_csv(args.test_xml_dir)
     
-    train_csv['relative_path'] = train_csv['filename'].apply(lambda x: os.path.dirname(os.path.relpath(os.path.join(args.train_xml_dir, x), args.train_image_dir)))
-    test_csv['relative_path'] = test_csv['filename'].apply(lambda x: os.path.dirname(os.path.relpath(os.path.join(args.test_xml_dir, x), args.test_image_dir)))
+    def find_relative_path(xml_dir, image_dir, filename):
+        rel_path = os.path.dirname(os.path.relpath(os.path.join(image_dir, filename), image_dir))
+        if not rel_path:
+            rel_path = "."
+            print(f"Filename: {filename}, XML Dir: {xml_dir}, Image Dir: {image_dir}, Relative Path: {rel_path}")
+        return rel_path
+
+
+    train_csv['relative_path'] = train_csv['filename'].apply(lambda x: find_relative_path(args.train_xml_dir, args.train_image_dir, x))
+    test_csv['relative_path'] = test_csv['filename'].apply(lambda x: find_relative_path(args.test_xml_dir, args.test_image_dir, x))
+
     
     train_csv.to_csv(os.path.join(args.output_dir, 'train_labels.csv'), index=None)
     test_csv.to_csv(os.path.join(args.output_dir, 'test_labels.csv'), index=None)
