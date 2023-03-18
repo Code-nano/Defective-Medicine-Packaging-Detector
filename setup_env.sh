46f72f872b3a530abb1a7a843d1e4582f3b6f5f3
Code-nano <83939407+Code-nano@users.noreply.github.com>
2023-03-18 19:58:50 +0800

    updated

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,7 @@
-# after creating the environment using the following command:
+# after creating and activating the environment using the following command:
 # conda env create -f environment.yml
 # conda activate data-science
+# continue with this command:
 # bash setup_env.sh
 
 conda install -y -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
