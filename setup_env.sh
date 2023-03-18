03aaf59ca24c5172207bd676a8a9594885a216aa
Code-nano <83939407+Code-nano@users.noreply.github.com>
2023-03-18 20:19:27 +0800

    update

--- setup_env.sh
+++
@@ -1,8 +0,0 @@
-# after creating and activating the environment using the following command:
-# conda env create -f environment.yml
-# conda activate data-science
-# continue with this command:
-# bash setup_env.sh
-
-conda install -y -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
-python -m pip install "tensorflow<2.11"
